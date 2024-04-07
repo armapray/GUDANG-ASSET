a7008741b68d53fd0d3d7113c90ab06f29a19389
armapray <onyprayogi281@gmail.com>
2024-04-07 16:55:06 +0700

    revisi login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 //route resource
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
 
 // route login
-Route::get('/', 'LoginController@login')->name('login');
-Route::post('loginaksi', 'LoginController@loginaksi')->name('loginaksi');
-Route::get('home', 'HomeController@index')->name('home')->middleware('auth');
-Route::get('logoutaksi', 'LoginController@logoutaksi')->name('logoutaksi')->middleware('auth');
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('loginaksi', [LoginController::class, 'loginaksi'])->name('loginaksi');
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('logoutaksi', [LoginController::class, 'logoutaksi'])->name('logoutaksi')->middleware('auth');
+
 
 
